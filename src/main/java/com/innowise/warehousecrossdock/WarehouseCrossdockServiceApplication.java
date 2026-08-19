ac910302563c5946b1b3f21b7d8a5619d90238ea
lexavodoprovod <danikhololeenko@gmail.com>
2026-08-19 14:09:20 +0300

    init commit

---
+++ src/main/java/com/innowise/warehousecrossdock/WarehouseCrossdockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.warehousecrossdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseCrossdockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseCrossdockServiceApplication.class, args);
+	}
+
+}
